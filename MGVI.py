5a6b056f4bd3c77a127791ae91ca346551c6737f
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-06-25 17:47:54 +0200

    realistic params

--- MGVI.py
+++ MGVI.py
@@ -38,27 +38,112 @@ i3 = int(100/1200 * n)
 seed = 42
 key = random.PRNGKey(seed)
 
-roh_1 = jft.LogNormalPrior(0.09, 0.03, name="roh_1", shape=(1,))
-sigma_1 = jft.LogNormalPrior(12., 10., name="sigma_1", shape=(1,))
-roh_dm = jft.LogNormalPrior(0.02, 0.04, name="roh_dm", shape=(1,))
+rohs = jnp.array([  0.021, 0.016, 0.012, 
+                    0.0009, 0.0006, 0.0031, 
+                    0.0015, 0.0020, 0.0022, 
+                    0.007, 0.0135, 0.006, 
+                    0.002, 0.0035, 0.0001])
+
+sigmas = jnp.array([4., 7., 9., 
+                    40., 20., 7.5, 
+                    10.5, 14., 18., 
+                    18.5, 18.5, 20., 
+                    20., 37., 100.])
+
+erohs = jnp.array([ 0.5, 0.5, 0.5,
+                    0.5, 0.2, 0.2,
+                    0.2, 0.2, 0.2,
+                    0.2, 0.2, 0.2,
+                    0.2, 0.2, 0.2]) * rohs
+
+esigmas = jnp.array([   1., 1., 1.,
+                        1., 2., 2.,
+                        2., 2., 2.,
+                        2., 2., 5.,
+                        5., 5., 10.])
+
+for k in range(15):
+    exec(f'roh_{k+1} = jft.NormalPrior(rohs[{k}], erohs[{k}], name="roh_{k+1}".format({k}), shape=(1,))')
+    exec(f'sigma_{k+1} = jft.NormalPrior(sigmas[{k}], esigmas[{k}], name="sigma_{k+1}".format({k}), shape=(1,))')
+
+roh_dm = jft.UniformPrior(0, 0.2, name="roh_dm", shape=(1,))
 
 class ForwardModel(jft.Model):
     def __init__(self):
-        self.roh_1 = roh_1
-        self.sigma_1 = sigma_1
+        for k in range(15):
+            exec(f'self.roh_{k+1} = roh_{k+1}')
+            exec(f'self.sigma_{k+1} = sigma_{k+1}')
         self.roh_dm = roh_dm
 
         super().__init__(
-            init=self.roh_1.init | self.sigma_1.init | self.roh_dm.init
+            init=   self.roh_1.init | self.sigma_1.init | \
+                    self.roh_2.init | self.sigma_2.init | \
+                    self.roh_3.init | self.sigma_3.init | \
+                    self.roh_4.init | self.sigma_4.init | \
+                    self.roh_5.init | self.sigma_5.init | \
+                    self.roh_6.init | self.sigma_6.init | \
+                    self.roh_7.init | self.sigma_7.init | \
+                    self.roh_8.init | self.sigma_8.init | \
+                    self.roh_9.init | self.sigma_9.init | \
+                    self.roh_10.init | self.sigma_10.init | \
+                    self.roh_11.init | self.sigma_11.init | \
+                    self.roh_12.init | self.sigma_12.init | \
+                    self.roh_13.init | self.sigma_13.init | \
+                    self.roh_14.init | self.sigma_14.init | \
+                    self.roh_15.init | self.sigma_15.init | \
+                    self.roh_dm.init
         )
 
     def __call__(self, x):
         p1 = self.roh_1(x)
         p2 = self.sigma_1(x)
-        p3 = self.roh_dm(x)
+        p3 = self.roh_2(x)
+        p4 = self.sigma_2(x)
+        p5 = self.roh_3(x)
+        p6 = self.sigma_3(x)
+        p7 = self.roh_4(x)
+        p8 = self.sigma_4(x)
+        p9 = self.roh_5(x)
+        p10 = self.sigma_5(x)
+        p11 = self.roh_6(x)
+        p12 = self.sigma_6(x)
+        p13 = self.roh_7(x)
+        p14 = self.sigma_7(x)
+        p15 = self.roh_8(x)
+        p16 = self.sigma_8(x)
+        p17 = self.roh_9(x)
+        p18 = self.sigma_9(x)
+        p19 = self.roh_10(x)
+        p20 = self.sigma_10(x)
+        p21 = self.roh_11(x)
+        p22 = self.sigma_11(x)
+        p23 = self.roh_12(x)
+        p24 = self.sigma_12(x)
+        p25 = self.roh_13(x)
+        p26 = self.sigma_13(x)
+        p27 = self.roh_14(x)
+        p28 = self.sigma_14(x)
+        p29 = self.roh_15(x)
+        p30 = self.sigma_15(x)
+        p31 = self.roh_dm(x)
 
-        def complicated_function(roh_1, sigma_1, roh_dm):
-            params = jnp.array([[roh_1[0], sigma_1[0]], ])
+        def complicated_function(roh_1, sigma_1, roh_2, sigma_2, 
+                                 roh_3, sigma_3, roh_4, sigma_4, 
+                                 roh_5, sigma_5, roh_6, sigma_6, 
+                                 roh_7, sigma_7, roh_8, sigma_8, 
+                                 roh_9, sigma_9, roh_10, sigma_10, 
+                                 roh_11, sigma_11, roh_12, sigma_12, 
+                                 roh_13, sigma_13, roh_14, sigma_14, 
+                                 roh_15, sigma_15, roh_dm):
+            params = jnp.array([
+                    [roh_1[0], sigma_1[0]], [roh_2[0], sigma_2[0]], 
+                    [roh_3[0], sigma_3[0]], [roh_4[0], sigma_4[0]], 
+                    [roh_5[0], sigma_5[0]], [roh_6[0], sigma_6[0]], 
+                    [roh_7[0], sigma_7[0]], [roh_8[0], sigma_8[0]], 
+                    [roh_9[0], sigma_9[0]], [roh_10[0], sigma_10[0]], 
+                    [roh_11[0], sigma_11[0]], [roh_12[0], sigma_12[0]], 
+                    [roh_13[0], sigma_13[0]], [roh_14[0], sigma_14[0]], 
+                    [roh_15[0], sigma_15[0]]])
             roh_dm = roh_dm[0]
 
             #Formulierung des Anfangswertproblems (z taucht in den Formeln auf, um an anderen DGLs zu testen)
@@ -131,7 +216,12 @@ class ForwardModel(jft.Model):
 
             return vdfo_norm_calc
 
-        return complicated_function(p1, p2, p3)
+        return complicated_function(p1, p2, p3, p4, p5, 
+                                    p6, p7, p8, p9, p10, 
+                                    p11, p12, p13, p14, p15, 
+                                    p16, p17, p18, p19, p20,
+                                    p21, p22, p23, p24, p25,
+                                    p26, p27, p28, p29, p30, p31)
 
 # This initialises your forward-model which computes something data-like
 fwd = ForwardModel()
@@ -204,19 +294,21 @@ samples, state = jft.optimize_kl(
 
 # Now the samples-object contains all the abstract parameters that were inferred
 # Reading out the physical input parameter values goes e.g. like this:
-roh1 = jft.mean_and_std(tuple(roh_1(s) for s in samples))
-sigma1 = jft.mean_and_std(tuple(sigma_1(s) for s in samples))
+for k in range(15):
+    exec(f'roh_{k+1} = jft.mean_and_std(tuple(roh_{k+1}(s) for s in samples))')
+    exec(f'sigma_{k+1} = jft.mean_and_std(tuple(sigma_{k+1}(s) for s in samples))')
 rohdm = jft.mean_and_std(tuple(roh_dm(s) for s in samples))
 
 # Please save your results in some way:
 # TODO
 
-print("Inferred values:", "MEAN:", roh1[0], "STD:", roh1[1])
-print("with the true value being:", roh_1(pos_truth))
-print('')
-print("Inferred values:", "MEAN:", sigma1[0], "STD:", sigma1[1])
-print("with the true value being:", sigma_1(pos_truth))
-print('')
+for k in range(15):
+    exec(f'print("Inferred values:", "MEAN:", roh{k+1}[0], "STD:", roh{k+1}[1])')
+    exec(f'print("with the true value being:", roh_{k+1}(pos_truth))')
+    exec(f'print("Inferred values:", "MEAN:", sigma{k+1}[0], "STD:", sigma{k+1}[1])')
+    exec(f'print("with the true value being:", sigma_{k+1}(pos_truth))')
+    exec(f'print("")')
+
 print("Inferred values:", "MEAN:", rohdm[0], "STD:", rohdm[1])
 print("with the true value being:", roh_dm(pos_truth))
 
