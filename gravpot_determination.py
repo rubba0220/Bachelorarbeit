d0ac47c6cf1f1567214050b248684ef8635ca73b
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-07-03 16:12:11 +0200

    first plots

--- gravpot_determination.py
+++ gravpot_determination.py
@@ -34,31 +34,6 @@ params = jnp.array([[0.021, 4.], [0.016, 7.], [0.012, 9.], \
                     [0.002, 20.], [0.0035, 37.], [0.0001, 100.]]) 
                                                         #mu(0) ??? in M_sun/pc^3, sigma(0) in km/s
 
-'''--------------------------------------------------------------------------------------'''
-#Routinen
-
-# jit(step)
-@partial(jit, static_argnames=['f', 'n'])
-def eigenerSolverV1(roh_dm, params, z0, u0, f, n, dz):
-                                                          
-    # Runge-Kutta 4. Ordnung
-    @partial(jit, static_argnames=['f']) #nötig ??
-    def rk4_step(roh_dm, params, z0, u0, dz, f):
-        k1 = dz * f(roh_dm, params, z0, u0)
-        k2 = dz * f(roh_dm, params, z0 + dz / 2, u0 + k1 / 2)
-        k3 = dz * f(roh_dm, params, z0 + dz / 2, u0 + k2 / 2)
-        k4 = dz * f(roh_dm, params, z0 + dz, u0 + k3)
-        u1 = u0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        return u1
-    
-    uz = []
-
-    for i in range(n):
-        u0 = rk4_step(roh_dm, params, z0+i*dz, u0, dz, f)
-        uz.append(u0)
-
-    return uz
-
 # lax.scan(step)
 @partial(jit, static_argnames=['f', 'n']) 
 def eigenerSolverV2(roh_dm, params, z0, u0, f, n, dz):
@@ -82,7 +57,7 @@ def eigenerSolverV2(roh_dm, params, z0, u0, f, n, dz):
     return uz
 
 # neu mit diffrax
-from diffrax import diffeqsolve, Dopri5, Tsit5, Dopri8, ODETerm, SaveAt, PIDController
+from diffrax import diffeqsolve, Dopri5, ODETerm, SaveAt, PIDController, DirectAdjoint
 
 # Dopri5
 @partial(jit, static_argnames=['f', 'n']) 
@@ -97,49 +72,75 @@ def diffraxDopri5(roh_dm, params, z0, u0, f, n, dz):
     sol = diffeqsolve(term, solver, t0=z0, t1=z0+n*dz, dt0=dz, y0=u0,
                         args=(roh_dm, params),
                     saveat=saveat,
-                    stepsize_controller=stepsize_controller)
+                    stepsize_controller=stepsize_controller,
+                    adjoint=DirectAdjoint())
 
     zs = sol.ts
     uz = sol.ys
 
     return uz
 
-# Tsit5
-@partial(jit, static_argnames=['f', 'n']) 
-def diffraxTsit5(roh_dm, params, z0, u0, f, n, dz):
+n = 1200
+dz = 1
 
-    vector_field = lambda z, y, args: f(args[0], args[1], z, y) #wrapper für reihenfolge
-    term = ODETerm(vector_field)
-    solver = Tsit5()
-    saveat = SaveAt(ts=jnp.linspace(0, n*dz, n))
-    stepsize_controller = PIDController(rtol=1e-5, atol=1e-5)
+rd = 0.02
+rds = jnp.linspace(0, 0.2, 10)
+rs = 0.01
+rss = jnp.linspace(0, 0.1, 10)
+s = 12.
+ss = jnp.linspace(2, 40, 10)
 
-    sol = diffeqsolve(term, solver, t0=z0, t1=z0+n*dz, dt0=dz, y0=u0,
-                        args=(roh_dm, params),
-                    saveat=saveat,
-                    stepsize_controller=stepsize_controller)
+fig, ax = plt.subplots(figsize=(20,10))
+plt.title('Rho_dm in einfachem Massenmodell')
+ax.set_xlabel('z / kpc')
+ax.set_ylabel('$\Phi$ / (km/s)$^2$')
+for rdi in rds:
+    p = jnp.array([[rs, s]])
+    uz = diffraxDopri5(rdi, p, z0, u0, f, n, dz)
+    plt.plot(jnp.linspace(0, n*dz, n), uz[:,0], label = f'rd = {rdi:.3f} M_sun/pc^3')
+ax.grid()
+ax.legend()
+fig.tight_layout()
 
-    zs = sol.ts
-    uz = sol.ys
+fig, ax = plt.subplots(figsize=(20,10))
+plt.title('Rho_s in einfachem Massenmodell')
+ax.set_xlabel('z / kpc')
+ax.set_ylabel('$\Phi$ / (km/s)$^2$')
+for rsi in rss:
+    p = jnp.array([[rsi, s]])
+    uz = diffraxDopri5(rd, p, z0, u0, f, n, dz)
+    plt.plot(jnp.linspace(0, n*dz, n), uz[:,0], label = f'rd = {rsi:.3f} M_sun/pc^3')
+ax.grid()
+ax.legend()
+fig.tight_layout()
 
-    return uz
+fig, ax = plt.subplots(figsize=(20,10))
+plt.title('Sigma in einfachem Massenmodell')
+ax.set_xlabel('z / kpc')
+ax.set_ylabel('$\Phi$ / (km/s)$^2$')
+for si in ss:
+    p = jnp.array([[rs, si]])
+    uz = diffraxDopri5(rd, p, z0, u0, f, n, dz)
+    plt.plot(jnp.linspace(0, n*dz, n), uz[:,0], label = f's = {si:.3f} km/s')
+ax.grid()
+ax.legend()
+fig.tight_layout()
 
-# Dopri8
-@partial(jit, static_argnames=['f', 'n']) 
-def diffraxDopri8(roh_dm, params, z0, u0, f, n, dz):
+fig, ax = plt.subplots(figsize=(20,10))
+plt.title('Sigma/Rho_s in einfachem Massenmodell')
+ax.set_xlabel('z / kpc')
+ax.set_ylabel('$\Phi$ / (km/s)$^2$')
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+i = 0
+for rsi in rss[3:6]:
+    c = colors[i]
+    for si in ss[6-i*2:10-i*2]:
+        p = jnp.array([[rsi, si]])
+        uz = diffraxDopri5(rd, p, z0, u0, f, n, dz)
+        plt.plot(jnp.linspace(0, n*dz, n), uz[:,0], color = c)
+    plt.figtext(0.1, 0.9-i*0.05, f'rs = {rsi:.3f} M_sun/pc^3', color = c)
+    i += 1
+ax.grid()
+ax.legend()
+fig.tight_layout()
 
-    vector_field = lambda z, y, args: f(args[0], args[1], z, y) #wrapper für reihenfolge
-    term = ODETerm(vector_field)
-    solver = Dopri8()
-    saveat = SaveAt(ts=jnp.linspace(0, n*dz, n))
-    stepsize_controller = PIDController(rtol=1e-5, atol=1e-5)
-
-    sol = diffeqsolve(term, solver, t0=z0, t1=z0+n*dz, dt0=dz, y0=u0,
-                        args=(roh_dm, params),
-                    saveat=saveat,
-                    stepsize_controller=stepsize_controller)
-
-    zs = sol.ts
-    uz = sol.ys
-
-    return uz
