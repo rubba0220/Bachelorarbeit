433192e1c47cd3a55bfb9c89088440837317304f
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-07-05 15:02:00 +0200

    some changes

--- MGVI.py
+++ MGVI.py
@@ -7,7 +7,7 @@ from functools import partial
 from matplotlib import pyplot as plt
 from scipy import constants as const
 import nifty8.re as jft
-from diffrax import diffeqsolve, Dopri5, ODETerm, SaveAt, PIDController, DirectAdjoint
+import diffrax as dif
 import pandas as pd
 
 import time
@@ -29,93 +29,94 @@ G = const.G / (3.0857E+16)**3 * 1.989E+30 * (3.0857E+13)**2
                             #Umrechnung in pc^3/M_sun/s^2 (grav pot in (km/s)^2)
                             #Umrechnung, sodass z in parsec
 
-n = 1200
-dz = 1.
-
-i1 = int(200/1200 * n)
-i2 = n
-i3 = int(100/1200 * n)
+n = 1201
+i_s = int(200/1200 * (n-1))
+i_n = int(100/1200 * (n-1))
 
 #Formulierung des Anfangswertproblems (z taucht in den Formeln auf, um an anderen DGLs zu testen)
-f = lambda roh_dm, params, z, u: jnp.array([u[1], \
-            4*jnp.pi*G * (jnp.sum(params[:,0]*jnp.exp(-u[0]/params[:,1]**2)) + roh_dm)])
+f = lambda rho_dm, params, z, u: jnp.array([u[1], \
+            4*jnp.pi*G * (jnp.sum(params[:,0]*jnp.exp(-u[0]/params[:,1]**2)) + rho_dm)])
 z0 = 0.
+z1 = 1200.
 u0 = jnp.array([0.,0.]) #freie Nullpunktswahl/Symmetrie
 
 #numerische Lösung (mittels Dopri5/rk4)
 @partial(jit, static_argnames=['f', 'n']) 
-def diffraxDopri5(roh_dm, params, z0, u0, f, n, dz):
+def diffraxDopri5(rho_dm, params, z0, z1, u0, f, n):
 
     vector_field = lambda z, y, args: f(args[0], args[1], z, y) #wrapper für reihenfolge
-    term = ODETerm(vector_field)
-    solver = Dopri5()
-    saveat = SaveAt(ts=jnp.linspace(0, n*dz, n))
-    stepsize_controller = PIDController(rtol=1e-3, atol=1e-6)
-    adjoint = DirectAdjoint()
+    term = dif.ODETerm(vector_field)
+    solver = dif.Dopri5()
+    saveat = dif.SaveAt(ts=jnp.linspace(0, z1, n))
+    stepsize_controller = dif.PIDController(rtol=1e-3, atol=1e-6)
+    adjoint = dif.DirectAdjoint()
 
-    sol = diffeqsolve(term, solver, t0=z0, t1=z0+n*dz, dt0=dz, y0=u0,
-                        args=(roh_dm, params),
-                    saveat=saveat,
-                    stepsize_controller=stepsize_controller,
-                    adjoint = adjoint, throw=False)
-                    #max_steps=65536)
+    sol = dif.diffeqsolve(  term, solver, 
+                            t0=z0, t1=z1, dt0=None, y0=u0, args=(rho_dm, params), 
+                            saveat=saveat,
+                            adjoint=adjoint,
+                            stepsize_controller=stepsize_controller) #throw=False, max_steps=None
 
-    #zs = sol.ts
+    zs = sol.ts
     uz = sol.ys
 
-    return uz
+    return uz, zs
 
-@partial(jit, static_argnames=['f', 'n']) 
-def eigenerSolverV2(roh_dm, params, z0, u0, f, n, dz):
-                                                        
+@partial(jit, static_argnames=['f', 'n'])
+def eigenerSolverV2(rho_dm, params, z0, z1, u0, f, n):
+    dz = (z1-z0)/(n-1)
+
     # Runge-Kutta 4. Ordnung
-    # @partial(jit, static_argnames=['f']) #nötig ??
-    def rk4_step(roh_dm, params, z0, u0, dz, f):
-        k1 = dz * f(roh_dm, params, z0, u0)
-        k2 = dz * f(roh_dm, params, z0 + dz / 2, u0 + k1 / 2)
-        k3 = dz * f(roh_dm, params, z0 + dz / 2, u0 + k2 / 2)
-        k4 = dz * f(roh_dm, params, z0 + dz, u0 + k3)
+    def rk4_step(rho_dm, params, z0, dz, u0, f):
+        k1 = dz * f(rho_dm, params, z0, u0)
+        k2 = dz * f(rho_dm, params, z0 + dz / 2, u0 + k1 / 2)
+        k3 = dz * f(rho_dm, params, z0 + dz / 2, u0 + k2 / 2)
+        k4 = dz * f(rho_dm, params, z0 + dz, u0 + k3)
         u1 = u0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
         return u1
 
-    def rk4_step_scan(u, i):
-        return rk4_step(roh_dm, params, z0+i*dz, u, dz, f), \
-            rk4_step(roh_dm, params, z0+i*dz, u, dz, f)
+    def rk4_step_scan(u, x):
+        return rk4_step(rho_dm, params, x, dz, u, f), \
+            rk4_step(rho_dm, params, x, dz, u, f)
 
-    _, uz = lax.scan(rk4_step_scan, u0, jnp.linspace(0, n*dz, n))
+    zs = jnp.linspace(z0, z1, n)
+    _, uz = lax.scan(rk4_step_scan, u0, zs[:-1])
+    uz = jnp.concatenate([jnp.array([u0]), uz], axis=0)
 
-    return uz
+    return uz, zs
 
 #Berechnung des tracer density drop off
-#neu:lax.scan()
-@partial(jit, static_argnames=['i1', 'i2', 'i3'])
-def vdfo_norm(i1, i2, i3, z0, dz, uz):
-    
+@partial(jit, static_argnames=['n', 'i_s', 'i_n'])
+def vdfo_norm(z0, z1, i_s, i_n, uz, n):
+    dz = (z1-z0)/(n-1)
+
     #mock velocity dispersion function
     def sigma(z):
         return 20. + 17.*z/1000. #z in pc, sigma in km/s
     
-    zs = jnp.linspace(z0+i1*dz, z0+(i2-1)*dz, i2-i1)
-    sigma_sq_norm = (sigma(zs)/sigma(z0+i3*dz))**(2)
-    zss = jnp.linspace(z0+i3*dz, z0+(i1-1)*dz, i1-i3)
-    sigmass = sigma(zss)
+    z = jnp.linspace(z0+i_s*dz, z1, n-i_s)
+    sigma_sq_norm = (sigma(z)/sigma(z0+i_n*dz))**(2)
+    z_ns = jnp.linspace(z0+i_n*dz, z0+(i_s-1)*dz, i_s-i_n)
+    sigmass = sigma(z_ns)
 
     exp_int = jnp.exp(-jnp.sum(\
                 sigmass**(-2) \
-                * jnp.array(uz)[i3:i1,1] * dz))
+                * jnp.array(uz)[i_n:i_s,1] * dz))
 
     def exp_int_step(exp_int, i):
         return exp_int * jnp.exp(-sigma(z0+i*dz)**(-2) * jnp.array(uz)[i,1] * dz), \
                 exp_int * jnp.exp(-sigma(z0+i*dz)**(-2) * jnp.array(uz)[i,1] * dz)
 
-    _, exp_int_list = lax.scan(exp_int_step, exp_int, jnp.arange(i1, i2, 1))
+    _, exp_int_list = lax.scan(exp_int_step, exp_int, jnp.arange(i_s, n-1, 1)) #letzter Punkt wird nicht genutzt (Riemannsumme links)
+
+    exp_int_list = jnp.concatenate([jnp.array([exp_int]), exp_int_list], axis=0)
 
     vdfo_norm_calc = jnp.multiply(sigma_sq_norm**(-1), jnp.array(exp_int_list))
 
-    return vdfo_norm_calc
+    return vdfo_norm_calc, z
 
 ''' Test des Algorithmus zur MGVI '''
-rohs = jnp.array([  0.021, 0.016, 0.012, 
+rhos = jnp.array([  0.021, 0.016, 0.012, 
                     0.0009, 0.0006, 0.0031, 
                     0.0015, 0.0020, 0.0022, 
                     0.007, 0.0135, 0.006, 
@@ -127,11 +128,11 @@ sigmas = jnp.array([4., 7., 9.,
                     18.5, 18.5, 20., 
                     20., 37., 100.])
 
-erohs = jnp.array([ 0.5, 0.5, 0.5,
+erhos = jnp.array([ 0.5, 0.5, 0.5,
                     0.5, 0.2, 0.2,
                     0.2, 0.2, 0.2,
                     0.2, 0.2, 0.2,
-                    0.2, 0.2, 0.2]) * rohs
+                    0.2, 0.2, 0.2]) * rhos
 
 esigmas = jnp.array([   1., 1., 1.,
                         1., 2., 2.,
@@ -139,99 +140,45 @@ esigmas = jnp.array([   1., 1., 1.,
                         2., 2., 5.,
                         5., 5., 10.])
 
-for k in range(15):
-    exec(f'roh_{k+1} = jft.LogNormalPrior(rohs[{k}], erohs[{k}], name="roh_{k+1}".format({k}), shape=(1,))')
-    exec(f'sigma_{k+1} = jft.LogNormalPrior(sigmas[{k}], esigmas[{k}], name="sigma_{k+1}".format({k}), shape=(1,))')
-
-roh_dm = jft.UniformPrior(0., 0.2, name="roh_dm", shape=(1,))
+rho_s = jft.LogNormalPrior(rhos, erhos, name="rho_s", shape=(15,))
+sigma_s = jft.LogNormalPrior(sigmas, esigmas, name="sigma_s", shape=(15,))
+rho_dm = jft.UniformPrior(0., 0.2, name="rho_dm", shape=(1,))
 
 class ForwardModel(jft.Model):
     def __init__(self):
-        for k in range(15):
-            exec(f'self.roh_{k+1} = roh_{k+1}')
-            exec(f'self.sigma_{k+1} = sigma_{k+1}')
-        self.roh_dm = roh_dm
+        self.rho_s = rho_s
+        self.sigma_s = sigma_s
+        self.rho_dm = rho_dm
 
-        super().__init__(
-            init =   self.roh_1.init | self.sigma_1.init | self.roh_2.init | self.sigma_2.init | \
-                    self.roh_3.init | self.sigma_3.init | self.roh_4.init | self.sigma_4.init | \
-                    self.roh_5.init | self.sigma_5.init | self.roh_6.init | self.sigma_6.init | \
-                    self.roh_7.init | self.sigma_7.init | self.roh_8.init | self.sigma_8.init | \
-                    self.roh_9.init | self.sigma_9.init | self.roh_10.init | self.sigma_10.init | \
-                    self.roh_11.init | self.sigma_11.init | self.roh_12.init | self.sigma_12.init | \
-                    self.roh_13.init | self.sigma_13.init | self.roh_14.init | self.sigma_14.init | \
-                    self.roh_15.init | self.sigma_15.init | self.roh_dm.init)
+        super().__init__(init =  self.rho_s.init| self.sigma_s.init | self.rho_dm.init)
 
+    @jit
     def __call__(self, x):
-        r1 = self.roh_1(x)
-        s1 = self.sigma_1(x)
-        r2 = self.roh_2(x)
-        s2 = self.sigma_2(x)
-        r3 = self.roh_3(x)
-        s3 = self.sigma_3(x)
-        r4 = self.roh_4(x)
-        s4 = self.sigma_4(x)
-        r5 = self.roh_5(x)
-        s5 = self.sigma_5(x)
-        r6 = self.roh_6(x)
-        s6 = self.sigma_6(x)
-        r7 = self.roh_7(x)
-        s7 = self.sigma_7(x)
-        r8 = self.roh_8(x)
-        s8 = self.sigma_8(x)
-        r9 = self.roh_9(x)
-        s9 = self.sigma_9(x)
-        r10 = self.roh_10(x)
-        s10 = self.sigma_10(x)
-        r11 = self.roh_11(x)
-        s11 = self.sigma_11(x)
-        r12 = self.roh_12(x)
-        s12 = self.sigma_12(x)
-        r13 = self.roh_13(x)
-        s13 = self.sigma_13(x)
-        r14 = self.roh_14(x)
-        s14 = self.sigma_14(x)
-        r15 = self.roh_15(x)
-        s15 = self.sigma_15(x)
-        rdm = self.roh_dm(x)
+        rs = self.rho_s(x)
+        ss = self.sigma_s(x)
+        rdm = self.rho_dm(x)
 
-        def complicated_function(roh_1, sigma_1, roh_2, sigma_2, 
-                                 roh_3, sigma_3, roh_4, sigma_4, 
-                                 roh_5, sigma_5, roh_6, sigma_6, 
-                                 roh_7, sigma_7, roh_8, sigma_8, 
-                                 roh_9, sigma_9, roh_10, sigma_10, 
-                                 roh_11, sigma_11, roh_12, sigma_12, 
-                                 roh_13, sigma_13, roh_14, sigma_14, 
-                                 roh_15, sigma_15, roh_dm):
+        def complicated_function(rho_s, sigma_s, rho_dm):
             params = jnp.array([
-                    [roh_1[0], sigma_1[0]], [roh_2[0], sigma_2[0]], 
-                    [roh_3[0], sigma_3[0]], [roh_4[0], sigma_4[0]], 
-                    [roh_5[0], sigma_5[0]], [roh_6[0], sigma_6[0]], 
-                    [roh_7[0], sigma_7[0]], [roh_8[0], sigma_8[0]], 
-                    [roh_9[0], sigma_9[0]], [roh_10[0], sigma_10[0]], 
-                    [roh_11[0], sigma_11[0]], [roh_12[0], sigma_12[0]], 
-                    [roh_13[0], sigma_13[0]], [roh_14[0], sigma_14[0]], 
-                    [roh_15[0], sigma_15[0]]])
-            roh_dm = roh_dm[0]
+                    [rho_s[0], sigma_s[0]], [rho_s[1], sigma_s[1]], [rho_s[2], sigma_s[2]],
+                    [rho_s[3], sigma_s[3]], [rho_s[4], sigma_s[4]], [rho_s[5], sigma_s[5]],
+                    [rho_s[6], sigma_s[6]], [rho_s[7], sigma_s[7]], [rho_s[8], sigma_s[8]],
+                    [rho_s[9], sigma_s[9]], [rho_s[10], sigma_s[10]], [rho_s[11], sigma_s[11]],
+                    [rho_s[12], sigma_s[12]], [rho_s[13], sigma_s[13]], [rho_s[14], sigma_s[14]]])
+            
+            rho_dm = rho_dm[0]
 
-            uz = diffraxDopri5(roh_dm, params, z0, u0, f, n, dz)
+            uz, zs = diffraxDopri5(rho_dm, params, z0, z1, u0, f, n)
 
-            vdfo_norm_calc = vdfo_norm(i1, i2, i3, z0, dz, uz)
+            vdfo_norm_calc, z = vdfo_norm(z0, z1, i_s, i_n, uz, n)
 
             return vdfo_norm_calc
 
-        return complicated_function(r1, s1, r2, s2, 
-                                    r3, s3, r4, s4, 
-                                    r5, s5, r6, s6, 
-                                    r7, s7, r8, s8, 
-                                    r9, s9, r10, s10, 
-                                    r11, s11, r12, s12, 
-                                    r13, s13, r14, s14, 
-                                    r15, s15, rdm)
+        return complicated_function(rs, ss, rdm)
 
 # This initialises your forward-model which computes something data-like
 fwd = ForwardModel()
-def test_mgvi(s, ns = 6):
+def test_mgvi(s):
     seed = s
     key = random.PRNGKey(seed)
 
@@ -244,24 +191,24 @@ def test_mgvi(s, ns = 6):
 
     key, subkey = random.split(key)
     noise_truth = (
-        (noise_cov(jft.ones_like(fwd.target))) ** 0.5 # sqrt to get from cov->std
-    ) * jft.random_like(key, fwd.target) # random means white noise
+        (noise_cov(jft.ones_like(fwd.target))) ** 0.5) * jft.random_like(subkey, fwd.target)
     data = fwd_truth + noise_truth
 
-    # #Visualisierung
-    # fig, ax = plt.subplots(figsize=(20,10))
-    # ax.set_xlabel('z/pc')
-    # #ax.set_yscale('log')
-    # ax.set_ylabel('$\\nu / \\nu_0 $')
-    # ax.scatter([0.+i*dz for i in range(i1,i2)], [data], marker='o')
-    # ax.grid()
-    # fig.tight_layout()
+    #Visualisierung
+    dz = (z1-z0)/(n-1)
+    z = jnp.linspace(z0+i_s*dz, z1, n-i_s)
+    fig, ax = plt.subplots(figsize=(20,10))
+    ax.set_xlabel('z/pc')
+    ax.set_ylabel('$\\nu / \\nu_0 $')
+    ax.scatter(z, data, marker='o')
+    ax.grid()
+    fig.tight_layout()
 
     lh = jft.Gaussian(data, noise_cov_inv).amend(fwd)
 
 
     # Now lets run the main inference scheme:
-    n_vi_iterations = ns
+    n_vi_iterations = 6
     delta = 1e-4
     n_samples = 10
 
@@ -306,33 +253,31 @@ def test_mgvi(s, ns = 6):
     results = {}
 
     for k in range(15):
-        exec(f'results["rohs{k+1}"] = tuple(roh_{k+1}(s).tolist()[0] for s in samples)')
-        exec(f'results["sigmas{k+1}"] = tuple(sigma_{k+1}(s).tolist()[0] for s in samples)')
-        exec(f'results["roh{k+1}"] = jft.mean_and_std(results["rohs{k+1}"])')
+        exec(f'results["rhos{k+1}"] = tuple(rho_s(s)[{k+1}].tolist() for s in samples)')
+        exec(f'results["sigmas{k+1}"] = tuple(sigma_s(s)[{k+1}].tolist() for s in samples)')
+        exec(f'results["rho{k+1}"] = jft.mean_and_std(results["rhos{k+1}"])')
         exec(f'results["sigma{k+1}"] = jft.mean_and_std(results["sigmas{k+1}"])')
-    results["rohsdm"] = tuple(roh_dm(s).tolist()[0] for s in samples)
-    results["rohdm"] = jft.mean_and_std(results["rohsdm"])
-    # Please save your results in some way:
-    # TODO
+    results["rhosdm"] = tuple(rho_dm(s).tolist()[0] for s in samples)
+    results["rhodm"] = jft.mean_and_std(results["rhosdm"])
 
-    truthr = [roh_1(pos_truth)[0], roh_2(pos_truth)[0], roh_3(pos_truth)[0], roh_4(pos_truth)[0], roh_5(pos_truth)[0], roh_6(pos_truth)[0], roh_7(pos_truth)[0], roh_8(pos_truth)[0], roh_9(pos_truth)[0], roh_10(pos_truth)[0], roh_11(pos_truth)[0], roh_12(pos_truth)[0], roh_13(pos_truth)[0], roh_14(pos_truth)[0], roh_15(pos_truth)[0], roh_dm(pos_truth)[0]]
-    meanr = [results[f'roh{k+1}'][0] for k in range(15)] + [results['rohdm'][0]]
-    stdr = [results[f'roh{k+1}'][1] for k in range(15)] + [results['rohdm'][1]]
+    truthr = [*rho_s(pos_truth), rho_dm(pos_truth)[0]]
+    meanr = [results[f'rho{k+1}'][0] for k in range(15)] + [results['rhodm'][0]]
+    stdr = [results[f'rho{k+1}'][1] for k in range(15)] + [results['rhodm'][1]]
 
 
-    data_roh = {
-        "True Value roh": truthr,
+    data_rho = {
+        "True Value rho": truthr,
 
-        "Inferred Value roh": meanr,
+        "Inferred Value rho": meanr,
 
-        "Standard Deviation roh": stdr,
+        "Standard Deviation rho": stdr,
 
-        "Samples roh": [results[f'rohs{k+1}'] for k in range(15)] + [results['rohsdm']],
+        "Samples rho": [results[f'rhos{k+1}'] for k in range(15)] + [results['rhosdm']],
 
-        "Abweichung roh": list((jnp.array(truthr) - jnp.array(meanr))/jnp.array(stdr))
+        "Abweichung rho": list((jnp.array(truthr) - jnp.array(meanr))/jnp.array(stdr))
     }
 
-    truths = [sigma_1(pos_truth)[0], sigma_2(pos_truth)[0], sigma_3(pos_truth)[0], sigma_4(pos_truth)[0], sigma_5(pos_truth)[0], sigma_6(pos_truth)[0], sigma_7(pos_truth)[0], sigma_8(pos_truth)[0], sigma_9(pos_truth)[0], sigma_10(pos_truth)[0], sigma_11(pos_truth)[0], sigma_12(pos_truth)[0], sigma_13(pos_truth)[0], sigma_14(pos_truth)[0], sigma_15(pos_truth)[0]]
+    truths = [*sigma_s(pos_truth)]
     means = [results[f'sigma{k+1}'][0] for k in range(15)]
     stds = [results[f'sigma{k+1}'][1] for k in range(15)]
 
@@ -349,10 +294,10 @@ def test_mgvi(s, ns = 6):
         "Abweichung sigma": list((jnp.array(truths) - jnp.array(means))/jnp.array(stds))
     }
 
-    dfr = pd.DataFrame(data_roh)
+    dfr = pd.DataFrame(data_rho)
     dfs = pd.DataFrame(data_sigma)
-    dfr.to_csv(f'data_roh_{ns}_un_50.csv', mode='a', header=False, index=False)
-    dfs.to_csv(f'data_sigma_{ns}_un_50.csv', mode='a', header=False, index=False)
+    dfr.to_csv(f'data_rho.csv', mode='a', header=False, index=False)
+    dfs.to_csv(f'data_sigma.csv', mode='a', header=False, index=False)
 
     t1 = time.time()
     print('Time:', t1-t0, 's')
@@ -361,7 +306,7 @@ seed = 100000
 key = random.PRNGKey(seed)
 
 key, subkey = random.split(key)
-seeds = random.randint(subkey, (50,), 1, 1000000)
+seeds = random.randint(subkey, (10,), 1, 1000000)
 
 def has_duplicates(arr):
     seen = set()
@@ -377,6 +322,6 @@ if has_duplicates(seeds):
 
 else:
     for s in seeds:
-        test_mgvi(s, 6)
+        test_mgvi(s)
 
 
---
+++ gravpot_determination_new.py
@@ -0,0 +1,143 @@
+#jax bisher nur für CPU intslliert (pip install -U "jax[cpu]")
+import jax
+import jax.numpy as jnp
+import jax.lax as lax
+from jax import jit, random
+from functools import partial
+from matplotlib import pyplot as plt
+from scipy import constants as const
+import nifty8.re as jft
+import diffrax as dif
+import pandas as pd
+
+import time
+jax.config.update("jax_enable_x64", True)
+
+t0 = time.time()
+
+# Plot-Formatierung
+plt.rcParams['font.size'] = 24.0
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Arial'
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['axes.labelsize'] = 'medium'
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.linewidth'] = 1.2
+plt.rcParams['lines.linewidth'] = 2.0
+
+G = const.G / (3.0857E+16)**3 * 1.989E+30 * (3.0857E+13)**2
+                            #Umrechnung in pc^3/M_sun/s^2 (grav pot in (km/s)^2)
+                            #Umrechnung, sodass z in parsec
+
+n = 1201
+i_s = int(200/1200 * (n-1))
+i_n = int(100/1200 * (n-1))
+
+#Formulierung des Anfangswertproblems (z taucht in den Formeln auf, um an anderen DGLs zu testen)
+f = lambda rho_dm, params, z, u: jnp.array([u[1], \
+            4*jnp.pi*G * (jnp.sum(params[:,0]*jnp.exp(-u[0]/params[:,1]**2)) + rho_dm)])
+z0 = 0.
+z1 = 1200.
+u0 = jnp.array([0.,0.]) #freie Nullpunktswahl/Symmetrie
+
+#numerische Lösung (mittels Dopri5/rk4)
+@partial(jit, static_argnames=['f', 'n']) 
+def diffraxDopri5(rho_dm, params, z0, z1, u0, f, n):
+
+    vector_field = lambda z, y, args: f(args[0], args[1], z, y) #wrapper für reihenfolge
+    term = dif.ODETerm(vector_field)
+    solver = dif.Dopri5()
+    saveat = dif.SaveAt(ts=jnp.linspace(0, z1, n))
+    stepsize_controller = dif.PIDController(rtol=1e-3, atol=1e-6)
+    adjoint = dif.DirectAdjoint()
+
+    sol = dif.diffeqsolve(  term, solver, 
+                            t0=z0, t1=z1, dt0=None, y0=u0, args=(rho_dm, params), 
+                            saveat=saveat,
+                            adjoint=adjoint,
+                            stepsize_controller=stepsize_controller) #throw=False, max_steps=None
+
+    zs = sol.ts
+    uz = sol.ys
+
+    return uz, zs
+
+@partial(jit, static_argnames=['f', 'n'])
+def eigenerSolverV2(rho_dm, params, z0, z1, u0, f, n):
+    dz = (z1-z0)/(n-1)
+
+    # Runge-Kutta 4. Ordnung
+    def rk4_step(rho_dm, params, z0, dz, u0, f):
+        k1 = dz * f(rho_dm, params, z0, u0)
+        k2 = dz * f(rho_dm, params, z0 + dz / 2, u0 + k1 / 2)
+        k3 = dz * f(rho_dm, params, z0 + dz / 2, u0 + k2 / 2)
+        k4 = dz * f(rho_dm, params, z0 + dz, u0 + k3)
+        u1 = u0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        return u1
+
+    def rk4_step_scan(u, x):
+        return rk4_step(rho_dm, params, x, dz, u, f), \
+            rk4_step(rho_dm, params, x, dz, u, f)
+
+    zs = jnp.linspace(z0, z1, n)
+    _, uz = lax.scan(rk4_step_scan, u0, zs[:-1])
+    uz = jnp.concatenate([jnp.array([u0]), uz], axis=0)
+
+    return uz, zs
+
+#Berechnung des tracer density drop off
+#neu:lax.scan()
+@partial(jit, static_argnames=['n', 'i_s', 'i_n'])
+def vdfo_norm(z0, z1, i_s, i_n, uz, n):
+    dz = (z1-z0)/(n-1)
+
+    #mock velocity dispersion function
+    def sigma(z):
+        return 20. + 17.*z/1000. #z in pc, sigma in km/s
+    
+    z = jnp.linspace(z0+i_s*dz, z1, n-i_s)
+    sigma_sq_norm = (sigma(z)/sigma(z0+i_n*dz))**(2)
+    z_ns = jnp.linspace(z0+i_n*dz, z0+(i_s-1)*dz, i_s-i_n)
+    sigmass = sigma(z_ns)
+
+    exp_int = jnp.exp(-jnp.sum(\
+                sigmass**(-2) \
+                * jnp.array(uz)[i_n:i_s,1] * dz))
+
+    def exp_int_step(exp_int, i):
+        return exp_int * jnp.exp(-sigma(z0+i*dz)**(-2) * jnp.array(uz)[i,1] * dz), \
+                exp_int * jnp.exp(-sigma(z0+i*dz)**(-2) * jnp.array(uz)[i,1] * dz)
+
+    _, exp_int_list = lax.scan(exp_int_step, exp_int, jnp.arange(i_s, n-1, 1))
+
+    exp_int_list = jnp.concatenate([jnp.array([exp_int]), exp_int_list], axis=0)
+
+    vdfo_norm_calc = jnp.multiply(sigma_sq_norm**(-1), jnp.array(exp_int_list))
+
+    return vdfo_norm_calc, z
+
+rho_dm = 0.025
+params = jnp.array([[0.021, 4.], [0.016, 7.], [0.012, 9.], \
+                    [0.0009, 40.], [0.0006, 20.], [0.0031, 7.5], \
+                    [0.0015, 10.5], [0.0020, 14.], [0.0022, 18.], \
+                    [0.007, 18.5], [0.0135, 18.5], [0.006, 20.], \
+                    [0.002, 20.], [0.0035, 37.], [0.0001, 100.]]) 
+
+uz, zs = diffraxDopri5(rho_dm, params, z0, z1, u0, f, n)
+vdfo_norm_calc, z = vdfo_norm(z0, z1, i_s, i_n, uz, n)
+
+''' Visualisierung'''
+fig, ax = plt.subplots(1, figsize=(20,10))
+ax.set_xlabel('z/pc')
+ax.set_ylabel('$\Phi / (km/s)^2$')
+ax.scatter(zs, [u[0] for u in uz], marker='o')
+ax.grid()
+fig.tight_layout()
+
+
+fig, ax = plt.subplots(figsize=(20,10))
+ax.set_xlabel('z/pc')
+ax.set_ylabel('$\\nu / \\nu_0 $')
+ax.scatter(z, vdfo_norm_calc, marker='o')
+ax.grid()
+fig.tight_layout()
