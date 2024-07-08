5929c3c6f060a87ff4430542c5b6dacc1667b075
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-07-08 11:21:14 +0200

    Plots ordentlich

--- gravpot_determination_new.py
+++ gravpot_determination_new.py
@@ -87,7 +87,6 @@ def eigenerSolverV2(rho_dm, params, z0, z1, u0, f, n):
     return uz, zs
 
 #Berechnung des tracer density drop off
-#neu:lax.scan()
 @partial(jit, static_argnames=['n', 'i_s', 'i_n'])
 def vdfo_norm(z0, z1, i_s, i_n, uz, n):
     dz = (z1-z0)/(n-1)
@@ -117,15 +116,7 @@ def vdfo_norm(z0, z1, i_s, i_n, uz, n):
 
     return vdfo_norm_calc, z
 
-
-
-rho_dm = 0.025
-params = jnp.array([[0.021, 4.], [0.016, 7.], [0.012, 9.], \
-                    [0.0009, 40.], [0.0006, 20.], [0.0031, 7.5], \
-                    [0.0015, 10.5], [0.0020, 14.], [0.0022, 18.], \
-                    [0.007, 18.5], [0.0135, 18.5], [0.006, 20.], \
-                    [0.002, 20.], [0.0035, 37.], [0.0001, 100.]]) 
-
+#binning
 @partial(jit, static_argnames=['n', 'i_s'])
 def binning(vdfo_norm_calc, z, n, i_s):
     l = int((n-i_s-1)/10)
@@ -145,28 +136,18 @@ def binning(vdfo_norm_calc, z, n, i_s):
 
     return integral, z_borders
 
+
+
+rho_dm = 0.025
+params = jnp.array([[0.021, 4.], [0.016, 7.], [0.012, 9.], \
+                    [0.0009, 40.], [0.0006, 20.], [0.0031, 7.5], \
+                    [0.0015, 10.5], [0.0020, 14.], [0.0022, 18.], \
+                    [0.007, 18.5], [0.0135, 18.5], [0.006, 20.], \
+                    [0.002, 20.], [0.0035, 37.], [0.0001, 100.]]) 
+
 uz, zs = diffraxDopri5(rho_dm, params, z0, z1, u0, f, n)
 vdfo_norm_calc, z = vdfo_norm(z0, z1, i_s, i_n, uz, n)
 integral, z_borders = binning(vdfo_norm_calc, z, n, i_s)
-
-''' Visualisierung binning'''
-fig, ax = plt.subplots(2, 1, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
-ax[0].set_xlabel('z/pc')
-ax[0].set_ylabel('$\\nu / \\nu_0 $ gebinnt')
-for i in range(len(z_borders)-1):
-    xmin = z_borders[i]
-    xmax = z_borders[i+1]
-    ax[0].hlines(integral[i], xmin, xmax, color='black')
-ax[0].scatter(z_borders[:-1], integral, marker='o', color='red')
-ax[0].grid()
-ax[0].legend()
-ax[1].set_xlabel('z/pc')
-ax[1].set_ylabel('$\\nu / \\nu_0 $')
-ax[1].scatter(z, vdfo_norm_calc, marker='o', color='blue')
-ax[1].grid()
-ax[1].legend()
-fig.tight_layout()
-fig.subplots_adjust(hspace=0.0)
 
 
 
@@ -200,6 +181,7 @@ uz_partial_inferred, zs_partial_inferred = diffraxDopri5(rho_dm_inferred, params
 vdfo_norm_calc_partial_inferred, z_partial_inferred = vdfo_norm(z0, z1, i_s, i_n, uz_partial_inferred, n)
 
 
+
 rhos_dm = jnp.unique(jnp.concatenate((jnp.round(jnp.linspace(0., 0.2, 11), decimals = 3), jnp.round(jnp.linspace(0.005, 0.03, 6), decimals=3))))
 
 erhos = jnp.array([ 0.5, 0.5, 0.5,
@@ -227,77 +209,101 @@ params_mm = params + jnp.transpose(jnp.vstack((-erhos, -esigmas)))
 
 
 
-
-# ''' Visualisierung fail'''
-# fig, ax = plt.subplots(2, 1, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
-# ax[0].set_xlabel('z/pc')
-# ax[0].set_ylabel('$\Phi / (km/s)^2$')
-# ax[0].scatter(zs_truth, [u[0] for u in uz_truth], marker='o', color='red', label='Truth')
-# ax[0].scatter(zs_inferred, [u[0] for u in uz_inferred], marker='x', color='black', label='Inferred')
-# ax[0].scatter(zs_partial_truth, [u[0] for u in uz_partial_truth], marker='o', color='blue', label='Partially Truth')
-# ax[0].scatter(zs_partial_inferred, [u[0] for u in uz_partial_inferred], marker='x', color='green', label='Partially Inferred')
-# ax[0].grid()
-# ax[1].set_xlabel('z/pc')
-# ax[1].set_ylabel('$\\nu / \\nu_0 $')
-# ax[1].scatter(z_truth, vdfo_norm_calc_truth, marker='o', color='red', label='Truth')
-# ax[1].scatter(z_inferred, vdfo_norm_calc_inferred, marker='x', color='black', label='Inferred')
-# ax[1].scatter(z_partial_truth, vdfo_norm_calc_partial_truth, marker='o', color='blue', label='Partially Truth')
-# ax[1].scatter(z_partial_inferred, vdfo_norm_calc_partial_inferred, marker='x', color='green', label='Partially Inferred')
-# ax[1].grid()
-# ax[1].legend()
-# fig.tight_layout()
-# fig.subplots_adjust(hspace=0.0)
-
-
-
-# ''' Visualisierung'''
-# fig, ax = plt.subplots(2, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
-# ax[0].set_xlabel('z/pc')
-# ax[0].set_ylabel('$\Phi / (km/s)^2$')
-# ax[1].set_xlabel('z/pc')
-# ax[1].set_ylabel('$\\nu / \\nu_0 $')
-
-# for rho_dm in rhos_dm:
-#     uz, zs = diffraxDopri5(rho_dm, params, z0, z1, u0, f, n)
-#     vdfo_norm_calc, z = vdfo_norm(z0, z1, i_s, i_n, uz, n)
-#     ax[0].scatter(zs, [u[0] for u in uz], label=f'$\\rho_dm = {rho_dm:.3f}$')
-#     ax[1].scatter(z, vdfo_norm_calc, label=f'$\\rho_dm = {rho_dm:.3f}$')
-
-# ax[0].grid()
-# ax[1].grid()
-# ax[1].legend(prop={'size': 23})
-# fig.tight_layout()
-# fig.subplots_adjust(hspace=0.0)
+''' Visualisierung fail'''
+fig, ax = plt.subplots(2, 1, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
+plt.title('Run with failed reconstruction of $\\rho_{dm}$')
+ax[0].set_xlabel('z/pc')
+ax[0].set_ylabel('$\Phi / (km/s)^2$')
+ax[0].scatter(zs_truth, [u[0] for u in uz_truth], marker='o', color='red', label='Truth')
+ax[0].scatter(zs_inferred, [u[0] for u in uz_inferred], marker='x', color='black', label='Inferred')
+ax[0].scatter(zs_partial_truth, [u[0] for u in uz_partial_truth], marker='o', color='blue', label='Partially Truth')
+ax[0].scatter(zs_partial_inferred, [u[0] for u in uz_partial_inferred], marker='x', color='green', label='Partially Inferred')
+ax[0].grid()
+ax[1].set_xlabel('z/pc')
+ax[1].set_ylabel('$\\nu / \\nu_0 $')
+ax[1].scatter(z_truth, vdfo_norm_calc_truth, marker='o', color='red', label='Truth')
+ax[1].scatter(z_inferred, vdfo_norm_calc_inferred, marker='x', color='black', label='Inferred')
+ax[1].scatter(z_partial_truth, vdfo_norm_calc_partial_truth, marker='o', color='blue', label='Partially Truth')
+ax[1].scatter(z_partial_inferred, vdfo_norm_calc_partial_inferred, marker='x', color='green', label='Partially Inferred')
+ax[1].grid()
+ax[1].legend()
+fig.tight_layout()
+fig.subplots_adjust(hspace=0.0)
 
 
 
-# ''' Visualisierung'''
-# fig, ax = plt.subplots(2, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
-# ax[0].set_xlabel('z/pc')
-# ax[0].set_ylabel('$\Phi / (km/s)^2$')
-# ax[1].set_xlabel('z/pc')
-# ax[1].set_ylabel('$\\nu / \\nu_0 $')
-# uz_pp, zs_pp = diffraxDopri5(rho_dm, params_pp, z0, z1, u0, f, n)
-# ax[0].scatter(zs_pp, [u[0] for u in uz_pp], label='pp')
-# vdfo_norm_calc_pp, z_pp = vdfo_norm(z0, z1, i_s, i_n, uz_pp, n)
-# ax[1].scatter(z_pp, vdfo_norm_calc_pp, label='pp')
-# uz_pm, zs_pm = diffraxDopri5(rho_dm, params_pm, z0, z1, u0, f, n)
-# ax[0].scatter(zs_pm, [u[0] for u in uz_pm], label='pm')
-# vdfo_norm_calc_pm, z_pm = vdfo_norm(z0, z1, i_s, i_n, uz_pm, n)
-# ax[1].scatter(z_pm, vdfo_norm_calc_pm, label='pm')
-# uz_mp, zs_mp = diffraxDopri5(rho_dm, params_mp, z0, z1, u0, f, n)
-# ax[0].scatter(zs_mp, [u[0] for u in uz_mp], label='mp')
-# vdfo_norm_calc_mp, z_mp = vdfo_norm(z0, z1, i_s, i_n, uz_mp, n)
-# ax[1].scatter(z_mp, vdfo_norm_calc_mp, label='mp')
-# uz_mm, zs_mm = diffraxDopri5(rho_dm, params_mm, z0, z1, u0, f, n)
-# ax[0].scatter(zs_mm, [u[0] for u in uz_mm], label='mm')
-# vdfo_norm_calc_mm, z_mm = vdfo_norm(z0, z1, i_s, i_n, uz_mm, n)
-# ax[1].scatter(z_mm, vdfo_norm_calc_mm, label='mm')
-# ax[0].grid()
-# ax[1].grid()
-# ax[1].legend()
-# fig.tight_layout()
-# fig.subplots_adjust(hspace=0.0)
+''' Visualisierung rho_dm'''
+fig, ax = plt.subplots(2, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
+plt.title('Influence of $\\rho_{dm}$')
+ax[0].set_xlabel('z/pc')
+ax[0].set_ylabel('$\Phi / (km/s)^2$')
+ax[1].set_xlabel('z/pc')
+ax[1].set_ylabel('$\\nu / \\nu_0 $')
+
+for rho_dm in rhos_dm:
+    uz, zs = diffraxDopri5(rho_dm, params, z0, z1, u0, f, n)
+    vdfo_norm_calc, z = vdfo_norm(z0, z1, i_s, i_n, uz, n)
+    ax[0].scatter(zs, [u[0] for u in uz], label=f'$\\rho_dm = {rho_dm:.3f}$')
+    ax[1].scatter(z, vdfo_norm_calc, label=f'$\\rho_dm = {rho_dm:.3f}$')
+
+ax[0].grid()
+ax[1].grid()
+ax[1].legend(prop={'size': 23})
+fig.tight_layout()
+fig.subplots_adjust(hspace=0.0)
+
+
+
+''' Visualisierung other params'''
+fig, ax = plt.subplots(2, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
+plt.title('Influence of other parameters')
+ax[0].set_xlabel('z/pc')
+ax[0].set_ylabel('$\Phi / (km/s)^2$')
+ax[1].set_xlabel('z/pc')
+ax[1].set_ylabel('$\\nu / \\nu_0 $')
+uz_pp, zs_pp = diffraxDopri5(rho_dm, params_pp, z0, z1, u0, f, n)
+ax[0].scatter(zs_pp, [u[0] for u in uz_pp], label='pp')
+vdfo_norm_calc_pp, z_pp = vdfo_norm(z0, z1, i_s, i_n, uz_pp, n)
+ax[1].scatter(z_pp, vdfo_norm_calc_pp, label='pp')
+uz_pm, zs_pm = diffraxDopri5(rho_dm, params_pm, z0, z1, u0, f, n)
+ax[0].scatter(zs_pm, [u[0] for u in uz_pm], label='pm')
+vdfo_norm_calc_pm, z_pm = vdfo_norm(z0, z1, i_s, i_n, uz_pm, n)
+ax[1].scatter(z_pm, vdfo_norm_calc_pm, label='pm')
+uz_mp, zs_mp = diffraxDopri5(rho_dm, params_mp, z0, z1, u0, f, n)
+ax[0].scatter(zs_mp, [u[0] for u in uz_mp], label='mp')
+vdfo_norm_calc_mp, z_mp = vdfo_norm(z0, z1, i_s, i_n, uz_mp, n)
+ax[1].scatter(z_mp, vdfo_norm_calc_mp, label='mp')
+uz_mm, zs_mm = diffraxDopri5(rho_dm, params_mm, z0, z1, u0, f, n)
+ax[0].scatter(zs_mm, [u[0] for u in uz_mm], label='mm')
+vdfo_norm_calc_mm, z_mm = vdfo_norm(z0, z1, i_s, i_n, uz_mm, n)
+ax[1].scatter(z_mm, vdfo_norm_calc_mm, label='mm')
+ax[0].grid()
+ax[1].grid()
+ax[1].legend()
+fig.tight_layout()
+fig.subplots_adjust(hspace=0.0)
+
+
+
+''' Visualisierung binning'''
+fig, ax = plt.subplots(2, 1, figsize=(20,20), sharex=True, gridspec_kw={'height_ratios': [1,1]})
+plt.title('Binning')
+ax[0].set_xlabel('z/pc')
+ax[0].set_ylabel('$\\nu / \\nu_0 $ in bins')
+for i in range(len(z_borders)-1):
+    xmin = z_borders[i]
+    xmax = z_borders[i+1]
+    ax[0].hlines(integral[i], xmin, xmax, color='black')
+ax[0].scatter(z_borders[:-1], integral, marker='o', color='red')
+ax[0].grid()
+ax[0].legend()
+ax[1].set_xlabel('z/pc')
+ax[1].set_ylabel('$\\nu / \\nu_0 $')
+ax[1].scatter(z, vdfo_norm_calc, marker='o', color='blue')
+ax[1].grid()
+ax[1].legend()
+fig.tight_layout()
+fig.subplots_adjust(hspace=0.0)
 
 
                                    
