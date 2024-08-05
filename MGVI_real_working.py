d722fa52ed670573ba6e04190307838ae2731f8a
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-08-05 12:34:54 +0200

    more iterations

--- MGVI_real_working.py
+++ MGVI_real_working.py
@@ -37,7 +37,7 @@ am_max = 6.724
 # am_min = 6.724
 # am_max = 7.400
 z2 = 100.
-z1 = 1000.
+z1 = 1200.
 z3 = 5000.
 
 bins = np.loadtxt(f'real data new intervals/bins_{am_min*1000:.0f}{am_max*1000:.0f}.txt')
@@ -53,8 +53,8 @@ bins = bins[i2:i1+1]
 n_bins = int(len(bins)-1)
 
 ''' Run '''
-name = 'n:smallere ' #['seeda ', 'seedb ', 'seedc ', 'seedd ', 'seede ']
-seed = 662 #[42, 80, 196, 371, 662] #80 macht Probleme #662
+name = 'n:smallerrora ' #['seeda ', 'seedb ', 'seedc ', 'seedd ', 'seede ']
+seed = 42 #[42, 80, 196, 371, 662] #80 macht Probleme #662
 interval = 'neg'
 
 poly = np.loadtxt(f'real data new intervals/poly_{am_min*1000:.0f}{am_max*1000:.0f}.txt')
@@ -204,7 +204,7 @@ R_sig2 = jft.Model(lambda x: x['sig2'], domain=fwd.target)
 
 lh_dfo = jft.Poissonian(data).amend(R_dfo)
 lh_sd = jft.Gaussian(49.4, lambda x: 1/(4.6)**2 * x).amend(R_sd)
-lh_sig2 = jft.Gaussian(v2, lambda x: 1/1100**2 * x).amend(R_sig2)	#7 #sinnvoller wählen !!!!
+lh_sig2 = jft.Gaussian(v2, lambda x: 1/110**2 * x).amend(R_sig2)	#7 #sinnvoller wählen !!!!
 
 lh = (lh_dfo + lh_sd + lh_sig2).amend(fwd)
 #lh_dfo + lh_sd + lh_sig2
