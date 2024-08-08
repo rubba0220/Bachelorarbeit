80205fde38d033b9ac35f8475b3a28e3f101550d
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-08-08 08:57:46 +0200

    other parameters

--- MGVI_real_velbin.py
+++ MGVI_real_velbin.py
@@ -52,8 +52,8 @@ bins = bins[i2:i1+1]
 n_bins = int(len(bins)-1)
 
 ''' Run '''
-name = 'n:it25f ' #['seeda ', 'seedb ', 'seedc ', 'seedd ', 'seede ', seedf]
-seed = 960 #[42, 80, 196, 371, 662, 960] #80 macht Probleme #662
+name = 'n:it12delta1e-2c ' #['seeda ', 'seedb ', 'seedc ', 'seedd ', 'seede ', seedf]
+seed = 196 #[42, 80, 196, 371, 662, 960] #80 macht Probleme #662
 interval = 'neg'
 
 poly = np.loadtxt(f'real data new intervals/poly_{am_min*1000:.0f}{am_max*1000:.0f}.txt')
@@ -214,7 +214,7 @@ lh = (lh_dfo + lh_sd + lh_sig2).amend(fwd)
 #lh_dfo + lh_sd + lh_sig2
 
 ''' Optimization '''
-n_vi_iterations = 25
+n_vi_iterations = 12
 delta = 1e-4
 n_samples = 10
 
