60c17bae1a20ae45ca15e75503526ea5b653af67
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2024-08-22 12:51:31 +0200

    ^...

--- real data colour/MGVI_real_colour_velbin.py
+++ real data colour/MGVI_real_colour_velbin.py
@@ -35,9 +35,9 @@ sigma_s = jft.LogNormalPrior(sigmas, esigmas, name="sigma_s", shape=(15,))
 rho_dm = jft.UniformPrior(0., 0.2, name="rho_dm", shape=(1,))
 
 ''' Domain '''
-subsample = 'upper_o1m4'
-z2 = 120.
-z1 = 1200.
+subsample = 'lower_o1m4'
+z2 = 200.
+z1 = 950.
 z3 = 5000.
 
 bins = np.loadtxt(f'data/bins_{subsample}.txt')
