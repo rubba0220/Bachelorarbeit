cec0dad246a1f9c0f40e4705ccef0706c1336d96
Ruben Bartel <ruben.bartel@rwth-aachen.de>
2025-02-03 12:16:17 +0100

    util_working correction

--- util_working.py
+++ util_working.py
@@ -19,14 +19,14 @@ f = lambda rho_dm, params, z, u: jnp.array([u[1], \
 z0 = 0. # starting point z=0 (galactic midplane)
 u0 = jnp.array([0.,0.]) # initial conditions on gravitational potential [free choice of ground level for condition on u[0] and symmetry condition (reflection at galactic midplane) on u[1]]
 
-f_tilt = lambda rho_dm, params, z, u, integral, domain: jnp.array([u[1], \
-            4*jnp.pi*G * (jnp.sum(params[:,0]*jnp.exp(-u[0]/params[:,1]**2)) * jnp.exp(-RegularGridInterpolator(domain, integral)[z]) + rho_dm)]) #integral is a correlated field modelling the tilt term in the Jeans equation, z is in 1 pc steps, so use z as index for integral
+f_tilt = lambda rho_dm, params, z, u, integral_tilt, domain: jnp.array([u[1], \
+            4*jnp.pi*G * (jnp.sum(params[:,0]*jnp.exp(-u[0]/params[:,1]**2)) * jnp.exp(-RegularGridInterpolator(domain, integral_tilt)[z]) + rho_dm)]) #integral is a correlated field modelling the tilt term in the Jeans equation, z is in 1 pc steps, so use z as index for integral
 
 # numerical solution of f from z=0 to z1 with n steps (in analysis realized to be 1pc steps and z1 to be integer)
 @partial(jit, static_argnames=['n']) 
-def diffraxDopri5(rho_dm, params, z1, n, integral, domain): # routine using Dopri5 from diffrax
+def diffraxDopri5(rho_dm, params, z1, n, integral_tilt, domain): # routine using Dopri5 from diffrax
 
-    vector_field = lambda z, y, args: f(args[0], args[1], z, y) #wrapper function
+    vector_field = lambda z, y, args: f_tilt(args[0], args[1], z, y, args[2], args[3]) #wrapper function
     term = dif.ODETerm(vector_field)
     solver = dif.Dopri5()
     saveat = dif.SaveAt(ts=jnp.linspace(0, z1, n))
@@ -34,7 +34,7 @@ def diffraxDopri5(rho_dm, params, z1, n, integral, domain): # routine using Dopr
     adjoint = dif.DirectAdjoint()
 
     sol = dif.diffeqsolve(  term, solver, 
-                            t0=z0, t1=z1, dt0=None, y0=u0, args=(rho_dm, params), 
+                            t0=z0, t1=z1, dt0=None, y0=u0, args=(rho_dm, params, integral_tilt, domain), 
                             saveat=saveat,
                             adjoint=adjoint,
                             stepsize_controller=stepsize_controller) 
@@ -95,9 +95,9 @@ def Solver(rho_dm, params, z1, z3, u3, n3): # u3 is the initial condition on u a
 def integrate(cf_vrz_R, cf_sig2, domain):
     y = cf_vrz_R/cf_sig2
     x = domain
-    integral = trapezoid(y, x)
+    integral_tilt = trapezoid(y, x)
 
-    return integral
+    return integral_tilt, domain
 
 # calculation of tracer density drop off from z2 to z1
 @partial(jit, static_argnames=['n', 'z1', 'z2'])
